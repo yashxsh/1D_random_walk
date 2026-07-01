bef4b12212cb2ea4f888f74f29f03d47b40ed535
Yash Sharma <yashxsh7@gmail.com>
2026-07-02 01:47:23 +0530

    added new thing

--- code.py
+++ code.py
@@ -18,3 +18,25 @@ plt.xlabel("final position")
 plt.ylabel("trials")
 plt.plot(d.keys(),d.values())
 plt.show()
+
+n = 1000000 #no of trials
+d = {}
+for i in range(n):
+  final = 0
+  for j in range(100):
+    a = r(0,2)
+    if a<2:
+      final+=1
+    else:
+      final-=1
+
+  if final in d :
+    d[final]+=1
+  else:
+    d[final]=1
+
+d = dict(sorted(d.items()))
+plt.xlabel("final position")
+plt.ylabel("trials")
+plt.plot(d.keys(),d.values())
+plt.show()
